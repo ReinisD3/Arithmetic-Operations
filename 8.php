7e542489525230b8ce3d749f0b1d36259b3c1b9c
ReinisD3 <reinis.detlavs.com>
2021-08-17 16:07:07 +0300

    Update calculation

--- 8.php
+++ 8.php
@@ -1,8 +1,8 @@
 <?php
 
-$arr_of_employees = [
+$employeesData = [
     ['Employee' => 'Employee 1','BasePay' => 7.5,'HoursWorked'=>35],
-    ['Employee' => 'Employee 2','BasePay' => 8.2,'HoursWorked'=>47],
+    ['Employee' => 'Employee 2','BasePay' => 10,'HoursWorked'=>41],
     ['Employee' => 'Employee 3','BasePay' => 10,'HoursWorked'=>73],
 ];
 
@@ -10,32 +10,34 @@ $arr_of_employees = [
 class WeeklyWages
 {
 
-    public static function weekly_pay_calculator(int $base_pay,int $hours_worked):string
+    public static function weeklyPayCalculator(float $basePay,int $hoursWorked):string
     {
-        if ($hours_worked > 60)
+        if ($hoursWorked > 60)
         {
-            return ': Error - worked over 60 hours'.PHP_EOL;
+            return ' Error - worked over 60 hours'.PHP_EOL;
+
         }
-        elseif ($base_pay < 8)
+        elseif ($basePay < 8)
         {
-            return ' : Error - base pay under 8$'.PHP_EOL;
+            return '  Error - base pay under 8$'.PHP_EOL;
+
         }
         else
         {
-            $salary = $base_pay*$hours_worked+($hours_worked-40)*0.5*$base_pay;
-            return ' earned '.$salary.' this week'.PHP_EOL;
+            $overtimeExtraPay = ($hoursWorked>40)?($hoursWorked-40)*0.5*$basePay : 0;
+            return 'earned :'.($basePay*$hoursWorked+$overtimeExtraPay).' this week'.PHP_EOL;
         }
     }
-    public static function employee_pay(array $data):void
+    public static function employeePay(array $data):void
     {
         foreach ($data as $employee)
         {
             echo $employee['Employee'].PHP_EOL;
-            echo WeeklyWages::weekly_pay_calculator($employee['BasePay'],$employee['HoursWorked']);
+            echo WeeklyWages::weeklyPayCalculator($employee['BasePay'],$employee['HoursWorked']);
         }
     }
 
 
 }
 
-WeeklyWages::employee_pay($arr_of_employees);
+WeeklyWages::employeePay($employeesData);
